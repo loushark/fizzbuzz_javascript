cbcdc06822f4b0d95c16553d7b330fa65a1b241c
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-04-07 16:26:06 +0100

    first jasmine test spec (red green)
    Co-Authored-By: Ralph Bartley <ralphgbartley@gmail.com>

---
+++ jasmine/jasmine-standalone-3.7.1/spec/fizzBuzzSpec.js
@@ -0,0 +1,12 @@
+describe('FizzBuzz',function() {
+  let fizzBuzz;
+
+  describe('knows when a number is', function(){
+    it('divisible by 3', function(){
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+  });
+
+});
