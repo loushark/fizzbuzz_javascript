782ab32dabde4308756abc0fd3f92143a17e08c8
ralphbartley <ralphgbartley@gmail.com>
2021-04-08 09:13:31 +0100

    fizzbuzzing red green test driven with jasmine

--- jasmine/jasmine-standalone-3.7.1/spec/fizzBuzzSpec.js
+++ jasmine/jasmine-standalone-3.7.1/spec/fizzBuzzSpec.js
@@ -1,12 +1,58 @@
 describe('FizzBuzz',function() {
   let fizzBuzz;
 
+  beforeEach(function(){
+    fizzBuzz = new FizzBuzz();
+  });
+
   describe('knows when a number is', function(){
     it('divisible by 3', function(){
-      fizzBuzz = new FizzBuzz();
+      //fizzBuzz = new FizzBuzz();
       expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+    it('not divisible by 3', function() {
+      //fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByThree(1)).toBe(false);
+    });
+
+    it('divisible by 5', function(){
+      //fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('not divisible by 5', function(){
+      //fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByFive(4)).toBe(false);
+    });
+
+    it('divisible by 15', function(){
+      //fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
+    it('not divisible by 15', function(){
+      //fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByFifteen(14)).toBe(false);
+    });
   });
+
+  describe('says Fizz when a number is', function(){
+    it('3', function(){
+      expect(fizzBuzz.says(3)).toMatch('Fizz');
+    });
+  });
+  describe('says Buzz when a number is', function(){
+    it('5', function(){
+      expect(fizzBuzz.says(5)).toEqual('Buzz');
+    });
+  });
+  describe('says FizzBuzz when a number is', function(){
+    it('15', function(){
+      expect(fizzBuzz.says(15)).toMatch('FizzBuzz');
+    });
+  });
+
+
 
 });
--- jasmine/jasmine-standalone-3.7.1/src/fizzBuzz.js
+++ jasmine/jasmine-standalone-3.7.1/src/fizzBuzz.js
@@ -1,5 +1,42 @@
 class FizzBuzz {
   isDivisibleByThree(number) {
-    return true;
+    return (number % 3 === 0);
   }
+
+  isDivisibleByFive(number) {
+    return (number % 5 === 0);
+  }
+
+  isDivisibleByFifteen(number) {
+    //return (number % 15 === 0);
+    if (this.isDivisibleByFive(number) && this.isDivisibleByThree(number)){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  says(number) {
+    if(this.isDivisibleByFifteen(number)){
+      return 'FizzBuzz';
+    }
+    else if(this.isDivisibleByThree(number)){
+      return 'Fizz';
+    }
+    else if(this.isDivisibleByFive(number)){
+      return 'Buzz';
+    }
+    else {
+      return number;
+    }
+  }
+
+  oneToOneHundred() {
+    for (let i = 0; i < 101; i++) {
+      console.log(this.says(i));
+    }
+  }
+
+
 }
